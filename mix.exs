2b4fc0019e4cd39c6fee848ae251ad75a8a89c1c
Erik Guzman <erik.guzman@guzman.codes>
2020-09-04 15:31:47 -0700

    Update mix.exs package info

--- mix.exs
+++ mix.exs
@@ -4,10 +4,14 @@ defmodule TalkLikeAX.MixProject do
   def project do
     [
       app: :talk_like_a_x,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      name: "Talk Like A (x)",
+      source_url: "https://github.com/CodingZeal/talk_like_a_x"
     ]
   end
 
@@ -23,10 +27,22 @@ defmodule TalkLikeAX.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:yaml_elixir, "~> 2.5"},
       {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
       {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
       {:deep_merge, "~> 1.0"}
+    ]
+  end
+
+  defp description() do
+    "Translates a string of words to a select number of lingos. Right now the only lingo available is Pirate, more to come shortly!"
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/CodingZeal/talk_like_a_x"}
     ]
   end
 end
