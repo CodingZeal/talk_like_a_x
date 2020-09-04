d37842b939a0e6b798a4d4ff00b000330518d072
Erik Guzman <erik.guzman@guzman.codes>
2020-09-04 15:05:08 -0700

    Doc update

--- lib/talk_like_ax.ex
+++ lib/talk_like_ax.ex
@@ -3,21 +3,36 @@ defmodule TalkLikeAX do
   alias TalkLikeAX.LingoLoader
 
   @moduledoc """
-  Documentation for `TalkLikeAX`.
+  TalkLikeAX provides you the ability convert a string of text into
+  a different lingo (AKA language).
   """
 
   @doc """
+  Returns a tuple with the string translated to default lingo pirate.
+
+  ## Parameters
+
+    - words: String of the words you would like tranlsated to a lingo
 
   ## Examples
 
       iex> TalkLikeAX.translate("hello friend")
       { :ok, "ahoy shipmate" }
+  """
+  def translate(words) when is_bitstring(words), do: translate(words, :pirate)
+
+  @doc """
+  Returns a tuple with the string transalated to your desired lingo.
+
+  ## Parameters
+
+    - words: String of the words you would like tranlsated to a lingo
+    - lingo: Atom for lingo you would like to translate to. :pirate is the only language with a robust dictionary of words.
+
+  ## Examples
 
       iex> TalkLikeAX.translate("hello friend", :pirate)
       { :ok, "ahoy shipmate" }
-
-      iex> TalkLikeAX.translate("Hello friend, doctorargus", :pirate, %{ "words" => %{ "doctorargus" => "DocArrrrgus" } })
-      { :ok, "Ahoy shipmate, DocArrrrgus" }
   """
   def translate(words, lingo \\ :pirate) when is_bitstring(words) do
     case LingoLoader.load_lingo(lingo) do
@@ -26,6 +41,20 @@ defmodule TalkLikeAX do
     end
   end
 
+  @doc """
+  Returns a tuple with the string transalated to your desired lingo.
+
+  ## Parameters
+
+    - words: String of the words you would like tranlsated to a lingo
+    - lingo: Atom for lingo you would like to translate to. :pirate is the only language with a robust dictionary of words.
+    - extended_lingo: A map containing two properties "words" & "gerunds" to add additional custom words to use in the translation. Both "words" & "gerunds" must be a map of "word" => "translated_word"
+
+  ## Examples
+
+      iex> TalkLikeAX.translate("Hello friend, doctorargus", :pirate, %{ "words" => %{ "doctorargus" => "DocArrrrgus" } })
+      { :ok, "Ahoy shipmate, DocArrrrgus" }
+  """
   def translate(words, lingo, extended_lingo) when is_map(extended_lingo) do
     case LingoLoader.load_lingo_and_extend(lingo, extended_lingo) do
       {:ok, lingo_map} -> {:ok, Translator.translate_to_lingo(words, lingo_map)}
